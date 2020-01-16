2b254ff0ecbcb9d72518e349ab115c2cfdca3bbe
SNx42 <seannowotny@live.de>
2020-01-16 10:47:01 +1000

    Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,66 @@
+module Main exposing (..)
+
+import Browser
+import Debug exposing (log)
+import Html exposing (button, div, input, text)
+import Html.Events exposing (onClick, onInput)
+import String exposing (fromInt, toInt)
+
+
+parseUserNumber text =
+    let
+        maybeText =
+            toInt text
+    in
+    case maybeText of
+        Just val ->
+            val
+
+        Nothing ->
+            0
+
+
+add a b =
+    a + b
+
+
+type Messages
+    = ClickedAdd
+    | ChangedAddText String
+
+
+init =
+    { value = 0, numberToAdd = 1 }
+
+
+view model =
+    div []
+        [ text (fromInt model.value)
+        , div [] []
+        , input [ onInput ChangedAddText ] []
+        , button [ onClick ClickedAdd ] [ text "Add" ]
+        ]
+
+
+update msg model =
+    let
+        log1 =
+            log "msg" msg
+
+        log2 =
+            log "model" model
+    in
+    case msg of
+        ClickedAdd ->
+            { model | value = model.value + model.numberToAdd }
+
+        ChangedAddText text ->
+            { model | numberToAdd = parseUserNumber text }
+
+
+main =
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
